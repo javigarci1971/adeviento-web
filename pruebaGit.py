305fec976fc8eaf7448d97cf2849b8c354d3c08e
javigarci1971 <81684739+javigarci1971@users.noreply.github.com>
2025-11-03 11:53:21 +0100

    Add remote change print statement

--- pruebaGit.py
+++ pruebaGit.py
@@ -2,6 +2,7 @@ print("Hola mundo" + " desde Python")
 print ("Rama de Javier")
 print ("Rama de Maria")
 print ("Otro print mas")
+print ("Cambio en remoto")
 print ("Print de prueba de idea")
 print("Este cambio no debería ir automaticamente a GitHub")
 
